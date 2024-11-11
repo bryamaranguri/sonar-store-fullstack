5697d359dbdee69c88b4a62ccdd766b4893fc1b3
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-11 00:21:24 -0500

    BestSeller component agregado

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium' >
 
-      <img src={assets.logo} className='w-36' alt='logo'  />
+      <Link to='/' ><img src={assets.logo} className='w-36' alt='logo'  /></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
@@ -19,7 +19,7 @@ const Navbar = () => {
         </NavLink>
 
         <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-          <p>CATÁLOGO</p>
+          <p>INSTRUMENTOS</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
