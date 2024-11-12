d317c2f176f974ac757881dc87e278cbec1179d1
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-12 18:20:42 -0500

    Agregar componente CartTotal y funcionalidad para calcular el total del carrito

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,16 +1,18 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from '../assets/assets';
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
   const currency = 'S/.'; // Moneda Sol Peruano
-  const delivery_fee = 10;
+  const delivery_fee = 0;
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
 
   const addToCart = async(itemId, size) => {
@@ -63,11 +65,29 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   }
 
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find(product => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += cartItems[items][item] * itemInfo.price;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return totalAmount;
+  }
+
   const value = {
     products , currency, delivery_fee,
     search, setSearch, showSearch, setShowSearch,
     cartItems, addToCart,
-    getCartCount, updateQuantity
+    getCartCount, updateQuantity,
+    getCartAmount, navigate
   }
 
   return (
