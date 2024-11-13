cbb16b2493a18598e25a8090eac84f6d50a63345
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-13 13:08:31 -0500

    Navbar corregido

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
 
-    const {setShowSearch, getCartCount } = useContext(ShopContext);
+    const {setShowSearch, navigate, getCartCount } = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 font-medium' >
@@ -32,14 +32,14 @@ const Navbar = () => {
         </NavLink>
 
         <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-          <p>CONTACTO</p>
+          <p>CONTÁCTANOS</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
 
       </ul>
 
       <div className='flex items-center gap-6'>
-        <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <img onClick={()=> {setShowSearch(true); navigate('/collection') }} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
         <div className='group relative'>
           <Link to='/login' ><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
