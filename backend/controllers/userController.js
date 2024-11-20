689b27da777ab1a346f880fe98af7bd9ed630178
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-19 22:37:55 -0500

    Añade userModel, autenticación de usuario, bcrypt y userRoute

---
+++ backend/controllers/userController.js
@@ -0,0 +1,82 @@
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
+
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+// Ruta para loguear un usuario
+const loginUser = async (req, res) => {
+
+  // const { email, password } = req.body;
+  // try {
+  //   const user
+
+  // } catch (error) {
+  //   console.log(error)
+  // }
+}
+
+
+// Ruta para registrar un usuario
+
+const registerUser = async (req, res) => {
+
+  try {
+    const { name, email, password } = req.body;
+
+    // Validar si el usuario ya existe
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ succes: false, message: 'El usuario ya existe' });
+    }
+
+    // Validar formato de email y contraseña fuerte
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: 'Inserte un Email válido por favor' });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: 'La contraseña debe tener al menos 8 caracteres' });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+
+
+
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Error al registrar el usuario' });
+  }
+}
+
+// Ruta para loggear admin
+const adminLogin = async (req, res) => {
+  // const { email, password } = req.body;
+  // try {
+  //   const admin
+  // }
+  // catch (error) {
+  //   console.log(error)
+  // }
+}
+
+
+export { loginUser, registerUser, adminLogin };
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
+
+// FIXME: Sizes
+
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  brand: { type: String, required: true },
-  category: { type: String, required: true },
-  countInStock: { type: Number, required: true },
   description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array, required: true },
+  bestseller: { type: Boolean },
+  date: { type: Number, default: Date.now },
 });
+
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+
+
+export default productModel;
