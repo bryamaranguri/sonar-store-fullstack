4275cfb0d6fe4bff184a8404b9701179d1841303
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-20 16:58:32 -0500

    Admin: Estructura de carpetas inicializado

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets} alt="" />
+      <button>Cerrar Sesión</button>
+
+    </div>
+  )
+}
+
+export default Navbar
