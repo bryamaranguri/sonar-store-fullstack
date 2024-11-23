56a9e7618d8cd06db38dea3545a6028f5fb2e5bf
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-11-22 22:24:40 -0500

    Autenticación con token en context y login y logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,15 @@ const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
 
-    const {setShowSearch, navigate, getCartCount } = useContext(ShopContext);
+    const {setShowSearch, navigate, getCartCount, token, setToken, setCartItems } = useContext(ShopContext);
+
+    const logout = () => {
+      navigate('/login');
+      localStorage.removeItem('token');
+      setToken('');
+      setCartItems({});
+
+    }
 
   return (
     <div className='flex items-center justify-between py-5 font-medium' >
@@ -42,15 +50,17 @@ const Navbar = () => {
         <img onClick={()=> {setShowSearch(true); navigate('/collection') }} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
         <div className='group relative'>
-          <Link to='/login' ><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
+          <img onClick={()=> token ? null : navigate('/login')} className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
 
+          {/* MENU DESPLEGABLE */}
+          { token &&
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded '>
               <p className='cursor-pointer hover:text-black'>Mi Perfil</p>
-              <p className='cursor-pointer hover:text-black'>Mis Órdenes</p>
-              <p className='cursor-pointer hover:text-black'>Cerrar Sesión</p>
+              <p onClick={()=> navigate('/orders')} className='cursor-pointer hover:text-black'>Mis Órdenes</p>
+              <p onClick={logout} className='cursor-pointer hover:text-black'>Cerrar Sesión</p>
             </div>
-          </div>
+          </div>}
         </div>
 
         <Link to='/cart' className='relative'>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
+
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -15,6 +16,7 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
   const [products, setProducts] = useState([]);
+  const [token, setToken] = useState('');
   const navigate = useNavigate();
 
 
@@ -107,12 +109,19 @@ const ShopContextProvider = (props) => {
     getProductsData();
   }, [])
 
+  useEffect(()=> {
+    if (!token && localStorage.getItem('token')) {
+      setToken(localStorage.getItem('token'));
+    }
+  },[])
+
   const value = {
     products , currency, delivery_fee,
     search, setSearch, showSearch, setShowSearch,
     cartItems, addToCart,
     getCartCount, updateQuantity,
-    getCartAmount, navigate, backendUrl
+    getCartAmount, navigate, backendUrl,
+    setToken, token
 
   }
 
