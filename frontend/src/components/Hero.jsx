1ed854580d0098d90e0aac1e2ae10887e1cf5f6e
Bryam Aranguri <bryam.aranguri.dev@gmail.com>
2024-12-10 13:26:05 -0500

    Corregir Hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,25 +2,33 @@ import { assets } from "../assets/assets"
 
 const Hero = () => {
   return (
-    <div className='flex flex-col sm:flex-row border rounded-sm'>
-      {/* Hero Lado Izquierdo */}
-      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0 bg-gradient-to-r from-[#152213] to-[#435151]'>
-          <div className='text-[#e8e0da]'>
-          <div className='flex items-center gap-2'>
-            {/* <p className='w-8 md:w-11 h-[2px] bg-[#e8e0da]'></p> */}
-            {/* <p className='font-medium text-sm md:text-base '>MÁS VENDIDOS</p> */}
+    <section className="hero-sec" >
+      <div className="single-item">
+        <div className="slide" data-color= "#afafaf" data-path="polygon" >
+          <img src={assets.hero_img} alt="" />
+          <div className="slide-text">
+            <h1 className='lora-regular text-9xl sm-py-3 lg:text-9xl leading-relaxed text-[#11100f] text-shadow-light'>Exclusividad Musical</h1>
+            <p className='font-semibold text-sm md:text-base text-shadow-dark underline text-[#efe9e1]'> VER CATÁLOGO</p>
           </div>
-          <h1 className='lora-regular text-3xl sm-py-3 lg:text-5xl leading-relaxed text-[#e8e0da]'>Exclusividad Musical</h1>
-          <div className='flex items-center gap-2 justify-end'>
-            <p className='font-semibold text-sm md:text-base'> COMPRAR AHORA</p>
-            <p className='w-8 md:w-11 h-[1px] bg-[#e8e0da]'></p>
+        </div>
 
+        <div className="slide" data-color= "#e8e0da" >
+          <img src={assets.hero_img2} alt="" />
+          <div className="slide-text">
+            <h1 className='lora-regular text-9xl sm-py-3 lg:text-9xl leading-relaxed text-[#e8e0da]  text-shadow-dark '>Calidad Inigualable</h1>
+            <p className='font-semibold text-sm md:text-base text-shadow-light underline text-[#efe9e1] '> DESCUBRE MÁS</p>
+          </div>
+        </div>
+
+        <div className="slide" data-color= "#e8e0da" >
+          <img src={assets.hero_img3} alt="" />
+          <div className="slide-text">
+            <h1 className='lora-regular text-9xl sm-py-3 lg:text-9xl leading-relaxed text-[#11100f] text-shadow-light '>Sonido Perfecto</h1>
+            <p className='font-semibold text-sm md:text-base text-shadow-dark underline text-[#efe9e1]'> COMPRAR AHORA</p>
           </div>
         </div>
       </div>
-      {/* Hero Lado Derecho */}
-      <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
-    </div>
+    </section>
   )
 }
 
